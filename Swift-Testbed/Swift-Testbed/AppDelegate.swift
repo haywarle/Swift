244ee2e60fd80062cc475719a033a45e91390adc
Lee <haywarle@icloud.com>
2015-05-12 18:39:51 +0100

    Created swift testbed

---
+++ Swift-Testbed/Swift-Testbed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift-Testbed
+//
+//  Created by Lee Hayward on 12/05/2015.
+//  Copyright (c) 2015 Lee Hayward. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift-Testbed/Swift-Testbed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift-Testbed
+//
+//  Created by Lee Hayward on 12/05/2015.
+//  Copyright (c) 2015 Lee Hayward. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
